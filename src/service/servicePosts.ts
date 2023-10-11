3b89638c9d51d03e907e15c38b963b5f7708a8e7
Salma-Raf <rksalma6@gmail.com>
2023-10-11 15:44:04 +0100

    5 commit

--- src/service/servicePosts.ts
+++ src/service/servicePosts.ts
@@ -46,67 +46,14 @@ export class ServiceMessage {
     }
   }
 
-  async reactPost(req: any, res: any) {}
-
-  async save(req: any, res: any) {
-    // const user2 = (await this.trepoclient.getUserById(
-    //   req.body.receiver_id
-    // )) as User;
-    // const user1 = (await this.trepoclient.getUserById(
-    //   req.body.sender_id
-    // )) as User;
-    // if (user1 != null && user2 != null) {
-    //   await this.messageDB
-    //     .createMessage(req.body as message)
-    //     .then((e) => {
-    //       return res.status(200).send(e);
-    //     })
-    //     .catch((err) => console.log("Error in create Message", err));
-    // } else {
-    //   return res.status(500).send("users not found");
-    // }
-  }
-
-  async transfer_m(req: any, res: any) {
-    // const { msg_id, usr_receiver } = req.body;
-    // const message = await this.messageDB.getMessageById(msg_id);
-    // const user2 = (await this.trepoclient.getUserById(usr_receiver)) as User;
-    // const user1 = (await this.trepoclient.getUserById(
-    //   message.sender_id
-    // )) as User;
-    // console.log(user1, user2, message);
-    // if (!user1 || !user2 || !message) {
-    //   return res.status(500).send("users or message not found.");
-    // } else {
-    //   await this.messageDB
-    //     .transferMessage(message, usr_receiver)
-    //     .then((e) => {
-    //       return res.status(200).send(e);
-    //     })
-    //     .catch((err) => console.log(err));
-    // }
-  }
-
-  async updatemssage(req: any, res: any) {
-    // try {
-    //   const msg: message = req.body as message;
-    //   const user1 = await this.trepoclient.getUserById(msg.sender_id);
-    //   const user2 = await this.trepoclient.getUserById(msg.sender_id);
-    //   if (!user1 || !user2) {
-    //     return req.status(500).send("usersnotfound");
-    //   }
-    //   const mes = await this.messageDB.getMessageById(req.params.id);
-    //   if (!mes) return res.status(500).send("msg not found");
-    //   console.log(mes);
-    //   const result = await this.messageDB.updateMessage(req.params.id, msg);
-    //   console.log(result);
-    //   if (result) {
-    //     return res.status.send(result);
-    //   } else {
-    //     throw new Error("impossible");
-    //   }
-    // } catch (e) {
-    //   return res.status(500).send(e);
-    // }
+  async reactPost(req: any, res: any) {
+    const post_id = req.params.id;
+    this.postDB
+      .reactPost(post_id)
+      .then((e) => {
+        if (e) return res.status(204).send(e);
+        return res.status(500).send(e);
+      })
+      .catch((err) => console.log(err));
   }
 }
